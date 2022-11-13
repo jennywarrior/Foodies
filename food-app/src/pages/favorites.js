099ed46e49e5336dafd3d88d84d48cb74654820f
vvyn <vicavyn3@gmail.com>
2022-11-13 06:07:23 -0600

    fix fav btn

--- food-app/src/pages/favorites.js
+++ food-app/src/pages/favorites.js
@@ -1,34 +1,18 @@
 import NavBar from './../components/NavBar';
 import './favorites.css';
-import Modal from '../components/modal';
-import React, { useState } from 'react';
+import React from 'react';
+import Favs from '../components/favs';
 
 function Profile() {
-  const [show, setShow] = useState(false);  
-  const [liked, setLiked] = useState(false);
-  let list = [];
-  for (let i = 0; i < 5; i++) {
-    list.push(
-      <div>
-        <div>
-          {!liked && <div style={{color: "black", fontSize: "35px", height: "0", width: "20px", position: "relative", right: "-5px", top: "-5px"}} onClick={() => setLiked(!liked)}>&#9829;</div> }
-          {liked && <div style={{color: "red", fontSize: "35px", height: "0", width: "20px", position: "relative", right: "-5px", top: "-5px"}} onClick={() => setLiked(!liked)}>&#9829;</div>}
-        </div>
-        <div className="Item" onClick={() => setShow(!show)}>
-          <div className='Name'>Very Awesome Restaurant</div>
-          <div className="Distance">3.2 miles away - Open until 9 PM</div>
-          <img src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?cs=srgb&dl=pexels-ella-olsson-1640777.jpg&fm=jpg" alt=''></img>
-          
-        </div>
-      </div>
-    )
-  }
+  let list = [1, 2, 3, 4, 5];
+
     return (
       <div className='favorites'>
         <NavBar/>
-        {show && <Modal closeModal={setShow}/>}
         <div className="Restaurant">
-          {list}
+          {list.map(() => (
+            <Favs/>
+          ))}
         </div>
       </div>
     );
--- food-app/src/pages/home.js
+++ food-app/src/pages/home.js
@@ -1,44 +1,23 @@
 import "./home.css";
 import "@fontsource/jetbrains-mono";
 import NavBar from "../components/NavBar";
-import React, { useState } from "react";
-import Modal from "../components/modal";
-import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import React from "react";
+import Favs from '../components/favs';
 
-function Home({closeModal}) {
-  const [show, setShow] = useState(false);  
-  const [liked, setLiked] = useState(false);
-
-
-  
-  let list = [];
-  for (let i = 0; i < 5; i++) {
-    list.push(
-      <div>
-        <div>
-          {!liked && <div style={{color: "black", fontSize: "35px", height: "0", width: "20px", position: "relative", right: "-5px", top: "-5px"}} onClick={() => setLiked(!liked)}>&#9829;</div> }
-          {liked && <div style={{color: "red", fontSize: "35px", height: "0", width: "20px", position: "relative", right: "-5px", top: "-5px"}} onClick={() => setLiked(!liked)}>&#9829;</div>}
-        </div>
-        <div className="Item" onClick={() => setShow(!show)}>
-          <div className='Name'>Very Awesome Restaurant</div>
-          <div className="Distance">3.2 miles away - Open until 9 PM</div>
-          <img src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?cs=srgb&dl=pexels-ella-olsson-1640777.jpg&fm=jpg" alt=''></img>
-          
-        </div>
-      </div>
-    );
-  }
+function Home({closeModal}) { 
+  let list = [1, 2, 3, 4, 5];
 
   return (
     <>
       <div>
         <NavBar />
-        {show && <Modal closeModal={setShow} />}
         <div className="Home">
           <div className="Map"></div>
           <div className="Restaurants">
             <p>Restaurants Near You</p>
-            {list}
+            {list.map(() => (
+              <Favs/>
+            ))}
           </div>
         </div>
       </div>
