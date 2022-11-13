9f9ba7f82a6b79eb6446308fe4bf2783c0ee93cf
Vulpine <portillofernando92@gmail.com>
2022-11-12 23:12:03 -0600

    added authentication

--- food-app/src/pages/signup.js
+++ food-app/src/pages/signup.js
@@ -1,41 +1,69 @@
-function SignUp() {
+import { useAuth } from "../contexts/AuthContext";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function SignUp() {
+  const navigate = useNavigate();
+  const { currentUser, register } = useAuth();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  async function handleFormSubmit(e) {
+    e.preventDefault();
+
+    try {
+      await register(email, password);
+      navigate("/");
+      console.log(fullName);
+    } catch (e) {
+      console.log(e);
+      alert("Failed to register");
+    }
+  }
+
+  useEffect(() => {
+    if (currentUser) {
+      navigate("/");
+    }
+  }, [currentUser, navigate]);
+
   return (
     <div>
       <header>
         <p>foodies</p>
-        <form>
-            <div>
-                <label>
-                    <input type="text" placeholder="email" />
-                </label>
-            </div>
-            <div>
-                <label>
-                    <input type="text" placeholder="password" />
-                </label>
-            </div>
-            <div>
-                <label>
-                    <input type="text" placeholder="full name"/>
-                </label>
-            </div>
-            <div>
-                <button>hi</button>
-                <button>hi</button>
-                <button>hi</button>
-                <button>hi</button>
-            </div>
-            <div>
-                <button>hi</button>
-                <button>hi</button>
-                <button>hi</button>
-                <button>hi</button>
-            </div>
-            <input type="submit" value="Submit" />
+        <form onSubmit={handleFormSubmit}>
+          <div>
+            <label>
+              <input
+                type="text"
+                placeholder="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              <input
+                type="text"
+                placeholder="password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              <input
+                type="text"
+                placeholder="fullname"
+                onChange={(e) => setFullName(e.target.value)}
+              />
+            </label>
+          </div>
+          <button type="submit">Submit</button>
         </form>
       </header>
     </div>
   );
 }
-
-export default SignUp;
