9c33983599269349128cec8a4962b755c0c42fc9
jennywarrior <94247664+jennywarrior@users.noreply.github.com>
2022-11-13 03:13:26 -0600

    home page merging

--- food-app/src/index.js
+++ food-app/src/index.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-<<<<<<< HEAD
-import Home from './pages/home';
-=======
 import Login from './pages/login';
 import Home from './home';
 import SignUp from './pages/signup';
 import Profile from './pages/profile';
 import Favorites from './pages/favorites';
->>>>>>> main
 
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<<<<<<< HEAD
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
-=======
   <BrowserRouter>
     <Routes>
       <Route exact path="/" element={<Login/>}/>
@@ -30,7 +21,6 @@ root.render(
       <Route exact path="/favorites" element={<Favorites/>}/>
     </Routes>
   </BrowserRouter>
->>>>>>> main
 );
 
 // If you want to start measuring performance in your app, pass a function
